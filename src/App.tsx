f8bdc2e0f5b456b49821e9089f6c4437d862f7e3
Lance Klinger <lance.d.klinger@gmail.com>
2022-04-02 22:48:56 -0400

    Mechanics for entering guesses

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import { Component, createSignal } from 'solid-js';
+import { Component, createSignal, For, Show } from 'solid-js';
 
 const App: Component = () => {
-  const [name, setName] = createSignal('');
+  const [guess, setGuess] = createSignal("");
+  const [committedGuesses, setCommittedGuesses] = createSignal<string[]>([]);
 
   const handleKeyPress = (e: KeyboardEvent) => {
     if (e.key === "Backspace") {
-      setName(name().slice(0, name().length - 1));
+      setGuess(guess().slice(0, guess().length - 1));
     }
-    else if (/^[a-zA-Z0-9]$/.test(e.key.toLowerCase())) {
-      if (name().length < 5) {
-        setName(name() + e.key.toUpperCase());
+    else if (e.key === "Enter") {
+      if (guess().length === 5 && committedGuesses().length < 6) {
+        setCommittedGuesses([...committedGuesses(), guess()]);
+        setGuess("");
+      }
+    }
+    else if (/^[a-zA-Z0-9]$/.test(e.key.toLowerCase())
+      && committedGuesses().length < 6
+      && !(e.metaKey || e.ctrlKey)
+    ) {
+      if (guess().length < 5) {
+        setGuess(guess() + e.key.toUpperCase());
       }
     }
   };
@@ -18,13 +28,23 @@ const App: Component = () => {
 
   return (
     <div>
-      <h1>UNLOCODLE</h1>
-      <p>Name: {name()}</p>
-      <p>0: {name()[0]}</p>
-      <p>1: {name()[1]}</p>
-      <p>2: {name()[2]}</p>
-      <p>3: {name()[3]}</p>
-      <p>4: {name()[4]}</p>
+      <div>
+        <h1>UNLOCODLE</h1>
+        <For each={committedGuesses()}>
+          {guess => <p>{guess}</p>}
+        </For>
+
+        <Show when={committedGuesses().length < 6}>
+          <p>0: {guess()[0]}</p>
+          <p>1: {guess()[1]}</p>
+          <p>2: {guess()[2]}</p>
+          <p>3: {guess()[3]}</p>
+          <p>4: {guess()[4]}</p>
+          <For each={Array(5 - committedGuesses().length).fill(null)}>
+            {() => <p>_</p>}
+          </For>
+        </Show>
+      </div>
     </div>
   );
 };
