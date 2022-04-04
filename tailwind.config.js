7ca2a86eb243419f7011517d68055a3318060315
Lance Klinger <lance.d.klinger@gmail.com>
2022-04-04 09:41:04 -0400

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const { rule } = require("postcss");
+
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
