37604d021a5a203c57c9ed666307469563cebab2
Lance Klinger <lance.d.klinger@gmail.com>
2022-05-12 03:51:14 -0400

    Add number row to on-screen keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const App: Component = () => {
   // TODO: Create function to calculate game status based on last guess and number of guesses
   const [gameResult, setGameResult] = createSignal<"unfinished" | "win" | "loss">("unfinished");
   const keyboard: string[][] = [
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
     ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
     ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
     ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"],
