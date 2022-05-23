2ad0632551e7644d16ec545c9a398474a2362442
Lance Klinger <lance.d.klinger@gmail.com>
2022-05-23 01:39:04 -0400

    animate adding letter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@ const { rule } = require("postcss");
 
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: true, // or 'media' or 'class',
+  darkMode: true,
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pop: {
+          '0%': { transform: 'scale(110%)' },
+          '100%': { transform: 'scale(100%)' },
+        }
+      },
+      animation: {
+        add: 'pop .1s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
