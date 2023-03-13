e2e311f610e1ca6a9c6acfadd282565dde2fd924
Lance Klinger <lance.d.klinger@gmail.com>
2023-03-13 02:20:35 -0400

    remove ternary from cell animation styling

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -9,23 +9,34 @@ export const Cell = (props: {
 	const c = children(() => props.children)
 	const delay = props.index ? (props.index as number) * 175 : 0
 	const animationStatus =
-		c() === undefined ? "idle" : props.status == null ? "add" : "reveal"
+		c() === undefined ? "idle" : props.status == undefined ? "add" : "reveal"
+
+	console.log("animationStatus", animationStatus, c())
 
 	return (
 		<div
 			class={`flex h-14 w-14 items-center justify-center rounded-md border-2
-     text-3xl font-extrabold text-black dark:text-white sm:h-16 sm:w-16
+     border-gray-300 text-3xl font-extrabold text-black dark:border-gray-600 dark:text-white sm:h-16 sm:w-16
      ${
-				animationStatus === "add"
-					? "animate-add border-gray-400"
-					: animationStatus === "reveal"
-					? props.status === "match"
-						? "animate-reveal-match"
-						: props.status === "exists"
-						? "animate-reveal-exists"
-						: "animate-reveal-none"
-					: "border-gray-300 dark:border-gray-600"
-			}`}
+				animationStatus === "add" &&
+				"animate-add border-gray-400 dark:border-gray-500"
+			}
+	 ${
+			animationStatus === "reveal" &&
+			props.status === "match" &&
+			"animate-reveal-match"
+		}
+	 ${
+			animationStatus === "reveal" &&
+			props.status === "exists" &&
+			"animate-reveal-exists"
+		}
+	 ${
+			animationStatus === "reveal" &&
+			props.status === "no_match" &&
+			"animate-reveal-none"
+		}
+		`}
 			style={{ "animation-delay": delay + "ms" }}
 		>
 			{c()}
